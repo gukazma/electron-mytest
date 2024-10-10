78975147725d8d8cbec057aa57efab77837077e2
gukazma <gukazma@proton.me>
2024-10-10 17:09:28 +0800

    🐞 fix: 解决路径问题

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -22,7 +22,16 @@ import type { VcReadyObject } from 'vue-cesium/es/utils/types'
     window.electron.ipcRenderer.on('asynchronous-reply', (_event, arg) => {
       console.log(arg) // 在 DevTools 控制台中打印“pong”
       console.log("123123!!!")
-      readyObj.Cesium.Cesium3DTileset.fromUrl(arg).then(result => {
+      // 替换多余的斜杠为单个斜杠
+      let fixedPath = arg.replace(/(\/)+/g, '/');
+
+      // 检查路径分隔符，如果是正斜杠则替换为反斜杠
+      fixedPath = fixedPath.replace(/\//g, '\\');
+
+      // 构建最终的路径
+      let finalPath = 'file:///' + fixedPath;
+      console.log(finalPath) // 在 DevTools 控制台中打印“pong”
+      readyObj.Cesium.Cesium3DTileset.fromUrl(finalPath).then(result => {
       console.log(result)
       readyObj.viewer.scene.primitives.add(result)
       readyObj.viewer.zoomTo(result, new readyObj.Cesium.HeadingPitchRange(0.0, -1.0, 50))
